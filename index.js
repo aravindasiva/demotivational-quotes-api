19b0fae92c7c4a2aa3e69fedeac8b938d03bc771
aravindasiva <aravindcva@hotmail.com>
2021-11-15 21:07:57 +0000

    small changes

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require("./Schema/type-defs")
 const { resolvers } = require("./Schema/resolvers")
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers
+});
+  
 server.listen().then(({url}) => {
   console.log(`API is running at : ${url} :`)
 });
